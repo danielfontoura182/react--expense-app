4fac3e9e0a5b581d7d47ea3c9fcec0dda28046c1
Daniel Fontoura <fontouradaniel@hotmail.com>
2021-11-07 07:30:04 -0300

    test useState function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
+
   function clickHandler() {
+    setTitle('UPDATED!')
     console.log('CLICKED!')
   }
 
@@ -13,7 +17,7 @@ function ExpenseItem(props) {
         <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Edit Title</button>
